574e8fc14521afbf96d89830f5cc874a93e45c14
Hrics12 <111898293+Hrics12@users.noreply.github.com>
2024-04-30 12:01:37 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,12 @@
 function convertToAdjList(adjMatrix) {
+    const adList ={};
+    for (let i = 0; i < adjMatrix.lenght; i++){
+        adList[i] = [];
+        for ( let j = 0; j < adjMatrix[i].length;j++){
+            if (adjMatrix[i][j] === 1) {
+                adList[i].push(j);
+            }
+        }
+    }
     return adjMatrix;
 }
