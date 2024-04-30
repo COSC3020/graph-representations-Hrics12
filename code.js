53fde52236b775f3b4d90d8519f9ccf21425b206
Hrics12 <111898293+Hrics12@users.noreply.github.com>
2024-04-30 12:07:42 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 function convertToAdjList(adjMatrix) {
     const adList =[];// realized I was returning an object and not an arrary of arrays
-    for (let i = 0; i < adjMatrix.lenght; i++){
+    for (let i = 0; i < adjMatrix.length; i++){
         adList[i] = [];
         for ( let j = 0; j < adjMatrix[i].length;j++){
             if (adjMatrix[i][j] === 1) {
